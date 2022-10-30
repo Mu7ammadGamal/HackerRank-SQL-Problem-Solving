d33a075f79888e51ed4165f9ae78937292dd2293
Muhammad Gamal <47898196+Mu7ammadGamal@users.noreply.github.com>
2022-10-30 16:33:19 +0200

    Update 15 Days of Learning SQL.sql

--- Hard/15 Days of Learning SQL.sql
+++ Hard/15 Days of Learning SQL.sql
@@ -1,78 +1,83 @@
 -- MSSQL
 
+-- CTE1 to get total number of unique hackers who made at least 1 submission each day
+-- CTE2 to get the hacker who made maximum number of submissions each day
+
+WITH CTE1 AS (
+  select 
+    sd sd, 
+    count(*) uh 
+  from 
+    (
+      select 
+        submission_date sd, 
+        hacker_id hid, 
+        (
+          select 
+            count(distinct s2.submission_date) 
+          from 
+            submissions s2 
+          where 
+            s2.submission_date <= s1.submission_date 
+            and s2.hacker_id = s1.hacker_id
+        ) n 
+      from 
+        submissions s1 
+      group by 
+        submission_date, 
+        hacker_id
+    ) as t 
+  where 
+    n >= datediff(day, '2016-03-01', sd)+ 1 
+  group by 
+    sd
+), 
+CTE2 AS (
+  select 
+    sd, 
+    hid 
+  from 
+    (
+      select 
+        sd sd, 
+        hid hid, 
+        ns, 
+        DENSE_RANK() over (
+          partition by sd 
+          order by 
+            ns desc, 
+            hid asc
+        ) r 
+      from 
+        (
+          select 
+            submission_date sd, 
+            hacker_id hid, 
+            (
+              select 
+                count(s2.hacker_id) 
+              from 
+                submissions s2 
+              where 
+                s2.submission_date = s1.submission_date 
+                and s2.hacker_id = s1.hacker_id
+            ) ns 
+          from 
+            submissions s1 
+          group by 
+            submission_date, 
+            hacker_id
+        ) as t
+    ) t2 
+  where 
+    r = 1
+) 
 select 
-  t1.sd, 
-  t1.uh, 
-  t2.hid, 
+  CTE1.sd, 
+  CTE1.uh, 
+  CTE2.hid, 
   h.name 
 from 
-  (
-    select 
-      sd sd, 
-      count(*) uh 
-    from 
-      (
-        select 
-          submission_date sd, 
-          hacker_id hid, 
-          (
-            select 
-              count(distinct s2.submission_date) 
-            from 
-              submissions s2 
-            where 
-              s2.submission_date <= s1.submission_date 
-              and s2.hacker_id = s1.hacker_id
-          ) n 
-        from 
-          submissions s1 
-        group by 
-          submission_date, 
-          hacker_id
-      ) as t 
-    where 
-      n >= datediff(day, '2016-03-01', sd)+ 1 
-    group by 
-      sd
-  ) t1 
-  join (
-    select 
-      sd, 
-      hid 
-    from 
-      (
-        select 
-          sd sd, 
-          hid hid, 
-          ns, 
-          DENSE_RANK() over (
-            partition by sd 
-            order by 
-              ns desc, 
-              hid asc
-          ) r 
-        from 
-          (
-            select 
-              submission_date sd, 
-              hacker_id hid, 
-              (
-                select 
-                  count(s2.hacker_id) 
-                from 
-                  submissions s2 
-                where 
-                  s2.submission_date = s1.submission_date 
-                  and s2.hacker_id = s1.hacker_id
-              ) ns 
-            from 
-              submissions s1 
-            group by 
-              submission_date, 
-              hacker_id
-          ) as t
-      ) t2 
-    where 
-      r = 1
-  ) t2 on t1.sd = t2.sd 
-  join hackers h on h.hacker_id = t2.hid
+  CTE1 
+  join CTE2 on CTE1.sd = CTE2.sd 
+  join hackers h on h.hacker_id = CTE2.hid
